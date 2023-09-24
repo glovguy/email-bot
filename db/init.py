e93c9fe4a43efef6f1dbc0e7a645ede8a2dde1dc
Karl <karlsmith@bouzou.com>
2023-09-24 14:50:52 -0400

    Adds db init script

---
+++ db/init.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+def setup_database():
+    # Create a new SQLite database or connect to it if it exists
+    conn = sqlite3.connect('email_bot.db')
+    cursor = conn.cursor()
+
+    # Create the emails table with the subject field and parent foreign key
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS emails (
+        id INTEGER PRIMARY KEY,
+        sender TEXT NOT NULL,
+        subject TEXT NOT NULL,
+        content TEXT NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        parent_id INTEGER,
+        FOREIGN KEY (parent_id) REFERENCES emails(id)
+    )
+    ''')
+
+    # Create the documents table without the question and embedding attributes
+    # but with the content attribute
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS documents (
+        id INTEGER PRIMARY KEY,
+        email_id INTEGER,
+        content TEXT NOT NULL,
+        FOREIGN KEY (email_id) REFERENCES emails(id)
+    )
+    ''')
+
+    # Create the authorized_senders table
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS authorized_senders (
+        id INTEGER PRIMARY KEY,
+        email_address TEXT UNIQUE NOT NULL
+    )
+    ''')
+
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    setup_database()
